674ba068effcffd3245fcfa3b6fd1572964285a1
fidabspd <fidabspd@gmail.com>
2024-02-03 14:56:17 +0900

    feat: enable serialization and deserialization

--- protobuf_test_3.py
+++ protobuf_test_3.py
@@ -19,12 +19,12 @@ def parse_adressbook_pbtxt(pbtxt_filepath: str):
 address_book_from_pbtxt_file = parse_adressbook_pbtxt(PBTXT_FILEPATH)
 print("### address book (from pbtxt file):\n{}".format(address_book_from_pbtxt_file))
 
-# # Write the address book back to disk as serialized file.
-# with open(SERIALIZED_FILEPATH, "wb") as f:
-#     f.write(address_book_from_pbtxt_file.SerializeToString())
+# Write the address book back to disk as serialized file.
+with open(SERIALIZED_FILEPATH, "wb") as f:
+    f.write(address_book_from_pbtxt_file.SerializeToString())
 
-# # Read the existing address book.
-# address_book_from_serialized_file = addressbook_pb2.AddressBook()
-# with open(SERIALIZED_FILEPATH, "rb") as f:
-#     address_book_from_serialized_file.ParseFromString(f.read())
-# print("### address book (from serialized file):\n{}".format(address_book_from_serialized_file))
+# Read the existing address book.
+address_book_from_serialized_file = addressbook_pb2.AddressBook()
+with open(SERIALIZED_FILEPATH, "rb") as f:
+    address_book_from_serialized_file.ParseFromString(f.read())
+print("### address book (from serialized file):\n{}".format(address_book_from_serialized_file))
